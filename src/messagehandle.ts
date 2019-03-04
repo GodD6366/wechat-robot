08c7566353e0350284c894d8131e7fc1486095ad
GodD6366 <daichangchun6366@gmail.com>
2019-03-04 13:06:00 +0800

    :sparkles: 增加图灵机器人

--- src/messagehandle.ts
+++ src/messagehandle.ts
@@ -1,30 +1,37 @@
 import Users from './user';
-
-let user: any;
+import talk from './turing/index';
 
 const messagehandle = async (message: any, users: Users) => {
-    if (/^ding$/i.test(message.text())) {
-        message.say(
-            `回复以下代码,执行对应的功能:\n"wx:phone" : 获取个人联系方式,\n"wx:mail" : 获取个人邮箱`
-        );
+    if (message.self()) {
+        return;
+    }
+    const contact = message.from();
+    if (/^help$/i.test(message.text())) {
+        message.say(`
+回复以下代码,执行对应的功能:
+"wx:phone" : 获取个人联系方式,
+"wx:mail" : 获取个人邮箱,
+"wx:ding [你要说的内容]" : 转发信息到主号,
+`);
     } else if (/^wx:phone$/i.test(message.text())) {
         message.say(`急事请拨打: 13175099932`);
     } else if (/^wx:mail$/i.test(message.text())) {
         message.say(`个人邮箱: daichangchun8@163.com`);
-    } else {
-        if (message.self()) {
-            return;
-        }
-
-        const contact = message.from();
+    } else if (/^wx:ding/i.test(message.text())) {
         if (contact.name() !== 'GodD') {
-            user = contact;
             let userGodD = await users.find('GodD');
-            userGodD.say(`${contact.name()}: ${message.text()}`);
+            let text = /^wx:ding (.*)/.exec(message.text())[1];
+            userGodD.say(`${contact.name()}: ${text}`);
         } else {
             // 转发给发件人
-            user.say(`${message.text()}`);
+            let [, userName, text] = /^wx:(.*) (.*)/.exec(message.text());
+            let user = await users.find(userName);
+            user && user.say(`${text}`);
         }
+    } else {
+        talk(message.text()).then(async text => {
+            contact.say(text);
+        });
     }
 };
 
---
+++ src/turing/index.ts
@@ -0,0 +1,35 @@
+const request = require('request');
+
+const talk = (text: string) => {
+    return new Promise((resolve, reject) => {
+        var options = {
+            method: 'POST',
+            url: 'http://openapi.tuling123.com/openapi/api/v2',
+            headers: {
+                'cache-control': 'no-cache'
+            },
+            body: JSON.stringify({
+                reqType: 0,
+                perception: {
+                    inputText: {
+                        text
+                    }
+                },
+                userInfo: {
+                    apiKey: '8a903d3283b5568e92a8dc1100cc591c',
+                    userId: 'OnlyUseAlphabet'
+                }
+            })
+        };
+
+        request(options, function(error: any, response: any, body: any) {
+            if (error) {
+                reject(new Error(error));
+            }
+            let text = body.results[0].values.text;
+            resolve(text);
+        });
+    });
+};
+
+export default talk;
